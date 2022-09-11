afd9ddac67580adca4ef838f5ec036066a798c05
Mikhail Sartakov <m.sartakov@g.nsu.ru>
2022-09-12 00:25:57 +0700

    Implemented resize and pushBack methods

--- vector.cpp
+++ vector.cpp
@@ -3,9 +3,9 @@ class vector {
   int size;
   int capacity;
 
-  vector(int capacity) {
+  public: vector(int capacity) {
     this->capacity = capacity;
-    this->size = size;
+    this->size = 0;
     this->data = new int[capacity]();
   }
 
@@ -13,20 +13,43 @@ class vector {
 
   // adds element to the last avaialable position,
   // resizes vector if current capacity is reached
-  void push_back(int num);
+  public: void pushBack(int num) {
+    if (this->size == this->capacity) {
+      if (this->capacity == 0) {
+        this->resize(1);
+      }
+      else {
+        this->resize(capacity * 2);
+      }
+    }
+    this->data[this->size++] = num;
+  }
+
   // resizes vector to given capacity
-  void resize(int new_capacity);
+  void resize(int newCapacity) {
+    int* newData = new int[newCapacity]();
+    for (int i = 0; i < capacity; ++i) {
+      newData[i] = data[i];
+    }
+    this->capacity = newCapacity;
+    delete[] this->data;
+    this->data = newData;
+  }
 
-  int size() { return this->size; }
-
-  void set(int num, int idx);
-  int at(int idx);
+  // void set(int num, int idx);
+  // int at(int idx);
 };
 
 int main(int argc, char **argv) {
-  // vector v;
-  // v.size();
-  // // size(&v)
-  // vector v1;
-  // v1.size();
+  vector* newVector = new vector(0);
+  newVector->pushBack(5);
+  newVector->pushBack(12);
+  newVector->pushBack(49);
+  newVector->pushBack(0);
+  newVector->pushBack(-345);
+  newVector->pushBack(1);
+  newVector->pushBack(42);
+  newVector->pushBack(904);
+  delete newVector;
+  return 0;
 }
