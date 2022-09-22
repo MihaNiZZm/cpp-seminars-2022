fbe2d22bace6ceb3b1214dbde85e63c05d5c3b88
Mikhail Sartakov <m.sartakov@g.nsu.ru>
2022-09-23 01:33:10 +0700

    Fixed operator= and copy ctor + fixed codestyle a bit

--- vector.cpp
+++ vector.cpp
@@ -1,51 +1,59 @@
+#include <algorithm>
 #include <iostream>
 
 class vector {
 public:
-  vector(int capacity_) {
-    this->capacity_ = capacity_;
-    this->size_ = 0;
-    this->data_ = new int[capacity_]();
+  vector() {
+    capacity_ = 8;
+    size_ = 0;
+    data_ = new int[capacity_]();
+  }
+
+  vector(int capacity) {
+    if (capacity == 0) {
+      std::cout << "Minimum capacity is 1. Capacity has been changed to 1 automatically." << std::endl;
+      capacity_ = 1;
+    }
+    capacity_ = capacity;
+    size_ = 0;
+    data_ = new int[capacity_]();
   }
 
   ~vector() {
     delete[] data_;
   }
 
-  vector(vector& other) {
-    this->capacity_ = other.capacity_;
-    this->size_ = other.size_;
-    this->data_ = new int[other.capacity_]();
-    for (int i = 0; i < other.capacity_; ++i) {
-      this->data_[i] = other.data_[i];
-    }
+  vector(const vector& other) {
+    capacity_ = other.capacity_;
+    size_ = other.size_;
+    data_ = new int[capacity_]();
+    std::copy(other.data_, other.data_ + capacity_, data_);
   }
 
   vector& operator=(const vector& other) {
-    this->capacity_ = other.capacity_;
-    this->size_ = other.size_;
-    if (this->data_ != nullptr) {
-      delete[] this->data_;
+    capacity_ = other.capacity_;
+    size_ = other.size_;
+    if (data_ != nullptr) {
+      delete[] data_;
     }
-    this->data_ = new int[other.capacity_]();
-    for (int i = 0; i < other.capacity_; ++i) {
-      this->data_[i] = other.data_[i];
-    }
+    data_ = new int[capacity_]();
+    std::copy(other.data_, other.data_ + capacity_, data_);
     return *this;
   }
 
   // adds element to the last avaialable position,
   // resizes vector if current capacity_ is reached
   void pushBack(int num) {
-    if (this->size_ == this->capacity_) {
-      if (this->capacity_ == 0) {
-        this->resize(1);
+    if (size_ == this->capacity_) {
+      if (capacity_ == 0) {
+        resize(1);
       }
       else {
-        this->resize(capacity_ * 2);
+        resize(capacity_ * 2);
       }
     }
-    this->data_[this->size_++] = num;
+    data_[size_] = num;
+    ++size_;
   }
 
   void printVector() {
@@ -74,32 +82,36 @@ private:
 };
 
 int main(int argc, char **argv) {
-  vector* v1 = new vector(0);
-  v1->pushBack(2);
-  v1->pushBack(-7);
-  v1->pushBack(99);
-  v1->pushBack(0);
-  v1->pushBack(1);
-  v1->pushBack(35);
+  vector v1 = vector();
+  v1.pushBack(2);
+  v1.pushBack(-7);
+  v1.pushBack(99);
+  v1.pushBack(0);
+  v1.pushBack(1);
+  v1.pushBack(35);
 
-  vector v2(*v1);
-  std::cout << "Copy ctor" << std::endl;
+  vector v2(v1);
   std::cout << "Vector v2" << std::endl;
   v2.printVector();
 
-  vector* v3 = new vector(0);
-  v3->operator=(*v1);
-  std::cout << "Operator=" << std::endl;
-  std::cout << "Vector v3" << std::endl;
-  v3->printVector();
-
-  v3->pushBack(80808);
-
+  v2.pushBack(737);
   std::cout << "Vector v1" << std::endl;
-  v1->printVector(); //v1 shouldn't change.
-  
+  v1.printVector();
+  std::cout << "Vector v2" << std::endl;
+  v2.printVector();
+
+  vector v3 = vector();
+  v3 = v1;
   std::cout << "Vector v3" << std::endl;
-  v3->printVector(); //v3 should change.
+  v3.printVector();
+
+  v3.pushBack(80808);
+  std::cout << "Vector v1" << std::endl;
+  v1.printVector(); //v1 shouldn't be changed.
+  std::cout << "Vector v2" << std::endl;
+  v2.printVector();
+  std::cout << "Vector v3" << std::endl;
+  v3.printVector(); //v3 should be changed.
   
   return 0;
 }
